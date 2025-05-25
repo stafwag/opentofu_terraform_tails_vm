d9b43a503b4bc4df84352034a5176dfefb4df899
Staf Wagemakers <staf@wagemakers.be>
2025-05-25 10:38:45 +0200

    Started with download code, defined triggers

--- modules/download_tails_iso/main.tf
+++ modules/download_tails_iso/main.tf
@@ -6,6 +6,21 @@ locals {
   sig_file_fullpath   = "${var.iso_path}/${local.sig_file_name}"
 }
 
+resource "null_resource" "tails_iso" {
+
+    triggers = {
+
+      tails_version       = "${local.tails_version}"
+      iso_file_name       = "${local.iso_file_name}"
+      sig_file_name       = "${local.sig_file_name}"
+      iso_file_fullpath   = "${local.iso_file_fullpath}"
+      sig_file_fullpath   = "${local.sig_file_fullpath}"
+
+    }
+
+}
+
+
 #
 # Using the abspath and pathexpand functions to support ~ with pathexpand
 # abspath to get the complete (absolute path)
