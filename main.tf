5bf9f917053a407e820aa04f053ef6df990984a3
Staf Wagemakers <staf@wagemakers.be>
2025-05-25 19:45:12 +0200

    Integrated download_tails_iso module
    * Defined locals
    * Integrated download_tails_iso module

--- main.tf
+++ main.tf
@@ -2,6 +2,25 @@ module "get_tails_latest_version" {
   source = "./modules/get_tails_latest_version"
 }
 
+locals {
+
+  depends_on = [ module.get_tails_latest_version ]
+
+  # tails_iso_path = "${abspath(path.root)}"
+  tails_iso_path = "~/Downloads/isos/tails"
+  tails_version = "${module.get_tails_latest_version.tails_version}"
+
+}
+
+module "download_tails_iso" {
+  source        = "./modules/download_tails_iso"
+
+  tails_version = "${local.tails_version}"
+  iso_path      = "${local.tails_iso_path}"
+
+}
+
+
 output "tails_version" {
   value = "${module.get_tails_latest_version.tails_version}"
 }
