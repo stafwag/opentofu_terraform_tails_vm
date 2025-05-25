a51fa71a06df175f8e0d3e955ace532a9746e8b8
Staf Wagemakers <staf@wagemakers.be>
2025-05-25 10:36:29 +0200

    Added abspath to the output variabeles

--- modules/download_tails_iso/main.tf
+++ modules/download_tails_iso/main.tf
@@ -6,16 +6,20 @@ locals {
   sig_file_fullpath   = "${var.iso_path}/${local.sig_file_name}"
 }
 
+#
+# Using the abspath and pathexpand functions to support ~ with pathexpand
+# abspath to get the complete (absolute path)
+#
 
 output "iso_file_fullpath" {
 
-  value = "${pathexpand(local.iso_file_fullpath)}"
+  value = "${abspath(pathexpand(local.iso_file_fullpath))}"
 
 }
 
 output "sig_file_fullpath" {
 
-  value = "${pathexpand(local.sig_file_fullpath)}"
+  value = "${abspath(pathexpand(local.sig_file_fullpath))}"
 
 }
 
