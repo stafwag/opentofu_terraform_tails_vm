253b7a4871ee8d82fea19babbae33cb0be5b66b1
Staf Wagemakers <staf@wagemakers.be>
2025-05-26 17:30:05 +0200

    Documentation update
    * Documentation update
    * Use a variable for the download dir
    * Set default download dir to ~/Downloads

--- main.tf
+++ main.tf
@@ -5,9 +5,6 @@ module "get_tails_latest_version" {
 locals {
 
   depends_on = [ module.get_tails_latest_version ]
-
-  # tails_iso_path = "${abspath(path.root)}"
-  tails_iso_path = "~/Downloads/isos/tails"
   tails_version = "${module.get_tails_latest_version.tails_version}"
 
 }
@@ -16,7 +13,7 @@ module "download_tails_iso" {
   source        = "./modules/download_tails_iso"
 
   tails_version = "${local.tails_version}"
-  iso_path      = "${local.tails_iso_path}"
+  iso_path      = "${var.tails_iso_path}"
 
 }
 
--- variables.tf
+++ variables.tf
@@ -1,17 +1,25 @@
 variable "name" {
-  description = "Virtual machine name"
-  default     = "mytails"
-  type        = string
+  description     = "Virtual machine name"
+  default         = "mytails"
+  type            = string
 }
 
 variable "memory" {
-  description = "Virtual machine memory"
-  default     = 4096
-  type        = number
+  description     = "Virtual machine memory"
+  default         = 4096
+  type            = number
 }
 
 variable "cpus" {
-  description = "Virtual machine vcpus"
-  default     = 2
-  type        = number
+  description     = "Virtual machine vcpus"
+  default         = 2
+  type            = number
+}
+
+
+variable "tails_iso_path" {
+  description     = "Tails iso download path"
+  # default = "${abspath(path.root)}"
+  default         = "~/Downloads/"
+  type            = string
 }
